b14338dbe8330715928429e84dc03b5839d5cdea
Vicente Guerra <vicenteguerrahdez@gmail.com>
2019-08-25 12:02:02 -0500

    [Update] Post Auth

--- routes/api/authRoute.js
+++ routes/api/authRoute.js
@@ -2,7 +2,7 @@ let Authcode = require('../../controllers/api/AuthcodeController').Authcode;
 
 module.exports = function (router) {
 
-    router.get('/auth', Authcode.auth);
+    router.post('/auth', Authcode.auth);
     router.post('/auth/request', Authcode.createCode);
     router.get('/r/:authcode_id', Authcode.redirectToApp);
 
